be5defdd81a210ceb27670709a0ee575421e5cea
chieze-franklin <chieze.franklin@gmail.com>
2021-02-23 15:10:26 +0100

    deploy to heroku

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const controllers = require('./controllers');
 
+router.get('/', function (req, res) {
+    res.json({
+        message: 'Hello, world!'
+    });
+});
+
 router.post('/subscribe/:topic', controllers.createSubscription);
 
 router.post('/publish/:topic', controllers.publishMessage);
